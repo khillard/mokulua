eee9fa97c91b542bbe6b3aa1fabeb158fac8e195
roch <33813863+the0roch0@users.noreply.github.com>
2022-10-24 17:26:43 -0700

    Update keymap.c

--- roch/keymap.c
+++ roch/keymap.c
@@ -8,7 +8,7 @@
 
 // Defines names for use in layer keycodes and the keymap
 enum layer_names {
-    _BASE = 0,
+    _BASE,
     _FN1,
 };
 
@@ -18,7 +18,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_SCRL,                             KC_Y,    KC_U,    KC_I,    KC_O,       KC_P,      KC_BSLS,
         KC_CAPS,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_INS,                         KC_CALC, KC_H,    KC_J,    KC_K,    KC_L,    KC_QUOT, KC_ENT,
         KC_LSFT, KC_SLASH, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_MUTE,    KC_MUTE, KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_RSFT,   KC_UP,
-        KC_LCTL, KC_LGUI, KC_LALT, mo(1),      KC_SPC, KC_SPC,       KC_FIND,   KC_PSCR,    KC_SPC, KC_SPC, mo(1),   KC_LEFT, KC_DOWN, KC_RGHT
+        KC_LCTL, KC_LGUI, KC_LALT, MO(1),      KC_SPC, KC_SPC,       KC_FIND,   KC_PSCR,    KC_SPC, KC_SPC, MO(1),   KC_LEFT, KC_DOWN, KC_RGHT
 	),
 	[_FN1] = LAYOUT(
         _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                         KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, KC_F12, KC_DEL,
@@ -33,6 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
     [_BASE] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_PGDN, KC_PGUP) },
     [_FN1]  = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS), ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },
+#endif
 };
 
 // Spaceship OLED Code Starts Here
@@ -309,5 +310,4 @@ bool oled_task_user(void) {
 
   return true;
 }
-#endif // OLED_ENABLE
 
